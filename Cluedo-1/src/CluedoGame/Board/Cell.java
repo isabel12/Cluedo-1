94975574012c2dbd58cb51257c73b4f7796d8a53
Isabel <isabel.broomenicholson@gmail.com>
2012-08-12 16:17:15 +1200

    Javadoc finished!

--- Cluedo-1/src/CluedoGame/Board/Cell.java
+++ Cluedo-1/src/CluedoGame/Board/Cell.java
@@ -9,10 +9,12 @@ import CluedoGame.Square;
 
 /**
  * The Cell class represents a square on the board that can be occupied by a Player's piece. 
- * It is primarily manipulated by the Board class.  The main functionality is that it has a set of neighbours to which 
- * it can travel to, allowing the board to be represented by a graph of Cells.
+ * It is primarily manipulated by the Board class.  The main functionality is that it has a 
+ * set of neighbours to which it can travel to, allowing the board to be represented by a graph 
+ * of Cells.
  * 
- * It can either be a RoomCell, or a CorridorCell (which could be an entrance to a room, a corridor square, or an intrigue square).
+ * It can either be a RoomCell, or a CorridorCell (which could be an entrance to a room, a corridor 
+ * square, or an intrigue square).
  * 
  * @author Izzi
  */
@@ -22,12 +24,13 @@ public abstract class Cell implements Square {
 
 	/**
 	 * Super constructor - this initiates the set of neighbours
+	 * @param - point (col, row)
 	 */
 	public Cell(Point position) {
 		neighbours = new HashSet<Cell>();
 		this.position = position;
 	}
-	
+
 	/**
 	 * Adds the given cell as a neighbour.  It won't add itself or null as a neighbour.
 	 * @param the cell to be added as the current Cell's neighbour.
@@ -39,7 +42,7 @@ public abstract class Cell implements Square {
 			throw new IllegalArgumentException("A cell cannot be it's own neighbour.");
 		if (neighbour == null)
 			throw new IllegalArgumentException("A cell cannot have null as a neighbour.");
-		
+
 		neighbours.add(neighbour);
 	}
 
@@ -48,14 +51,29 @@ public abstract class Cell implements Square {
 	 * Returns an unmodifiable version of the cell's neighbour cells.
 	 */
 	public Set<Cell> getNeighbours(){return Collections.unmodifiableSet(neighbours);}
-	
+
+
+	/**
+	 * Returns the position of the Cell on the Board as a Point (col, row).
+	 */
 	public Point getPosition(){
 		return this.position;
 	}
-		
-	public abstract void setBlocked(boolean isEmpty);  	// <--- this method is used when moving the player, so needs to be accessible from Cell.
-														// I don't think we need a getEmpty() method here.  We only use it for pathfinding, and that only uses CorridorCells.
+
+	/**
+	 * Sets the Cell as blocked.  This stops it being part of a path, or letting another 
+	 * player move to it.
+	 * @param isBlocked - whether the cell should be blocked or unblocked
+	 */
+	public abstract void setBlocked(boolean isBlocked);  	// <--- this method is used when moving the player, so needs to be accessible from Cell.
+
+
+
+	/**
+	 * Whether the cell is blocked or not.  If blocked, his stops it being part of a path, 
+	 * or letting another player move to it.
+	 */
 	public abstract boolean isBlocked();
-	
-	
+
+
 }
